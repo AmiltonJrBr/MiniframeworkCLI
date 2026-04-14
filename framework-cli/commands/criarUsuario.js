12fe26236e2359b6cd1703925831aac51162a44b
Amilton dos Santos Junior <101209321+AmiltonJrBr@users.noreply.github.com>
2026-04-14 19:46:17 -0300

    Add files via upload

---
+++ framework-cli/commands/criarUsuario.js
@@ -0,0 +1,8 @@
+/**
+ * Comando de exemplo para demonstrar recebimento de argumentos parseados.
+ */
+function criarUsuario(args) {
+  console.log("Usuário criado:", args.nome);
+}
+
+module.exports = criarUsuario;
